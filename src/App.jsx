1f364dcc9d3c743f90596e950275a14a149b05f9
Jose-Rojas-Jr <jr.joss1999@gmail.com>
2023-10-19 11:24:43 -0400

    segundo componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Nombre } from './componentes/atomos/nombre'
+import { Perfil } from './componentes/atomos/perfil'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,9 @@ function App() {
   return (
     <>
       <div>
+        <Perfil></Perfil>
         <Nombre></Nombre>
+        
       </div>
       
       
